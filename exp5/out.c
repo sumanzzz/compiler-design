7f5ab2105f113af7884b4eb3dab4cae155b37863
Reegan Suman <145418786+sumanzzz@users.noreply.github.com>
2026-01-30 15:24:44 +0530

    Add files via upload

---
+++ exp5/out.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	writef("Hello im Reegan");
+	readf("%d" ,&n);
+	writef("Number is %d",n);
+	return 0;
+}
---
+++ exp5/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	printf("Hello im Reegan");
+	scanf("%d" ,&n);
+	printf("Number is %d",n);
+	return 0;
+}
